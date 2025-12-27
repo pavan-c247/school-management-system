644d368fd8506f6ce793992f5569efee1b5933d3
pavansingh888 <psinghp888@gmail.com>
2025-12-27 14:48:25 +0530

    feat: add menu component

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
+import Image from "next/image";
+import Link from "next/link";
+import Menu from "@/components/Menu";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +18,20 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <div>dashboardLayout{children}</div>
+      <div className="h-screen flex">
+        {/* LEFT */}
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+          <Link 
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2"
+          >
+          <Image src='/logo.png' alt='Logo' width={32} height={32} />
+          <span className="hidden lg:block">SchoolEasy</span>
+          </Link>
+          <Menu/>
+        </div>
+        {/* RIGHT */}
+        <div className="w-[86%]  md:w-[92%] lg:w-[84%] xl:w-[86%]">right</div>
+        </div>
   );
 }
--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const menuItems = [
   {
     title: "MENU",
@@ -112,3 +115,28 @@ const menuItems = [
     ],
   },
 ];
+
+const Menu = () => {
+
+  return (
+    <div className="mt-4 text-sm flex flex-col">
+      {menuItems.map(i => (
+        <div className="flex flex-col gap-2" key={i.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
+          {i.items.map(item => (
+            <Link
+              key={item.label}
+              href={item.href}
+              className="flex justify-center items-center lg:justify-start gap-4 text-gray-500 py-2"
+            >
+              <Image src={item.icon} alt="" width={20} height={20} />
+              <span className="hidden lg:block">{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Menu;
