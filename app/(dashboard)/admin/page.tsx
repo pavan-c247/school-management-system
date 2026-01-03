705a561f3988f06c1399b92a77a301c3a432e356
pavan-c247 <pavan.s@chapter247.com>
2026-01-03 16:16:21 +0530

    feat: Announcement, EventCalendar, FinanceChart UI

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -1,5 +1,8 @@
+import Announcements from "@/components/Announcements"
 import AttendanceChart from "@/components/AttendanceChart"
 import CountChart from "@/components/CountChart"
+import EventCalendar from "@/components/EventCalendar"
+import FinanceChart from "@/components/FinanceChart"
 import UserCard from "@/components/UserCard"
 
 const AdminPage = () => {
@@ -27,11 +30,14 @@ const AdminPage = () => {
         </div>
         {/* BOTTOM CHART */}
         <div className="w-full h-[500px]">
-          {/* <FinanceChart /> */}
+          <FinanceChart />
         </div>
       </div>
       {/* RIGHT */}
-      <div className="w-full lg:w-1/3">r</div>
+      <div className="w-full lg:w-1/3 flex flex-col gap-8">
+      <EventCalendar />
+      <Announcements />
+      </div>
     </div>
   )
 }
